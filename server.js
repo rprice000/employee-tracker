b81f4f319c378b56a08837c44986cb2d3ea59a7c
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-13 14:23:46 -0600

    added npm express lines for server.js

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
+const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const db = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
     user: 'root',
     password: 'password',
-    database: 'employee_tracker',
+    database: 'employee_tracker_db',
   });
+
+  db.connect(err => {
+    if (err) throw err;
+    console.log("Please follow the prompts to access Employee Tracker Data");
+    accessEmployeeTracker();
+  });
+
+  const accessEmployeeTracker = () => {
+    inquirer.prompt({
+        message: 'From Here You Can View, Add, or Update Employee Data.  Select Exit If You Need to Quit The Application.',
+        name: 'menu',
+        type: 'list',
+        choices: [ 
+          'View All Departments',
+          'View All Jobs',
+          'View All Employees',
+          'Add Department',
+          'Add Job',
+          'Add Employee',
+          'Update Employee Data',
+          'Exit',
+        ],
+      })
+    };
