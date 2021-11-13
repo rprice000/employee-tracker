1863733425810b2445f8afe9e7adefaea1ec66df
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-13 16:52:17 -0600

    finished show all employee data

--- server.js
+++ server.js
@@ -39,31 +39,66 @@ const db = mysql.createConnection({
           'Exit',
         ],
       })
-    //   .then(response => {
-    //     switch (response.menu) {
-    //     case 'View all departments':
-    //       departmentData();
-    //       break;
-    //   }
-    // });
-    
-    
-
         .then(response => {
-          if(response.startData === 'View All Departments') {
-              departmentData()
+          if (response.startData === 'View All Departments') {
+              departmentList()
+          } else if (response.startData === 'View All Jobs') {
+             jobList()
+          } else if (response.startData === 'View All Employees') {
+             employeeList()
+          } else if(response.startData === 'Add Department') {
+              addDepartment()
           }
-      })
+        })
+
 
 
 
     };
 
-    const departmentData = () => {
-        db.query('SELECT * FROM department', function (err, res) {
+
+
+
+
+
+
+
+    const departmentList = () => {
+        db.query(`SELECT * FROM department`, function (err, res) {
           if (err) throw err;
           console.table(res);
           accessEmployeeTracker();
         });
       };
+
+      const jobList = () => {
+        db.query(`SELECT * FROM job`, function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          accessEmployeeTracker();
+        });
+      };
+
+      const employeeList = () => {
+        db.query(
+            `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      job.title, 
+                      department.department_name AS department,
+                      job.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+               FROM employee
+                      LEFT JOIN job ON employee.job_id = job.id
+                      LEFT JOIN department ON job.department_id = department.id
+                      LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+          function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            accessEmployeeTracker();
+          }
+        );
+      };
+
+      
 
